a907a347c268e7e391eb0c4bf15f5b03f38d3cc4
marks <marks@679a26be-e637-0410-a801-b7d8065a4221>
2010-10-18 16:19:10 +0000

    Added the inkmerge library made of the inkscape plugin and a test file, turned it into a package Added amethod to inkmerge to allow it be eb called by python Wrote a test for the above Wrote tests for the db-driven generate_sorecards function in the django project so now it will generate stuff based on in put from the model if you set it up right.
    git-svn-id: https://svn.aptivate.org/svn/reactionscorecards2/dev/reactionscorecards2/lib/inkmerge@3291 679a26be-e637-0410-a801-b7d8065a4221

---
+++ cmd.sh
@@ -0,0 +1,1 @@
+python invoke_from_python.py --data-file='../../resources/testdata.csv' --output='$file.svg'  ../../resources/simple-template.svg
---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest 
+from merge import Merger
+import os
+
+class test_invoke_merge(unittest.TestCase):
+    
+    def test_inkvok(self):
+        """ invoking the plugin with a template a data file from the 
+        fixtures shoudl produce an output file
+        """
+        template= 'fixtures/simple-template.svg'
+        datafile= 'fixtures/testdata.csv'
+        output_pattern= '/tmp/$file'
+        expected=  '/tmp/helloworld.svg'
+        if os.path.exists(expected):
+            os.remove(expected)
+        Merger().invoke( template, datafile, output_file_pattern=output_pattern)
+        self.assertTrue(os.path.exists(expected))
+
+if __name__ == '__main__':
+    unittest.main()
